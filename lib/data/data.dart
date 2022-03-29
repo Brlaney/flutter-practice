a656d822cd08094a2ba385240d49983a549d9541
brlaney42 <brlaney@outlook.com>
2022-03-28 21:05:06 -0400

    Adding main structure: Not tested yet

---
+++ lib/data/data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+
+final User currentUser = User(name: 'Marcus Ng', imageUrl: '');
+
+final List<User> onlineUsers = [
+  User(name: 'David Brooks', imageUrl: ''),
+  User(name: 'Jane Doe', imageUrl: ''),
+  User(name: 'Matthew Hinkle', imageUrl: ''),
+  User(name: 'Jimothy Pearson', imageUrl: ''),
+  User(name: 'Any Smith', imageUrl: ''),
+  User(name: 'Darius Rucker', imageUrl: ''),
+  User(
+    name: 'Tim McGraw',
+    imageUrl: '',
+  )
+];
